c92df78fc42ce0afeefddd5670cbcb2b018a0283
smadeira22 <saramariaramosmadeira@gmail.com>
2023-12-05 02:31:21 +0000

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import { ArtistInfo1 } from './ArtistInfo'
+import { ArtistInfo2 } from './ArtistInfo'
+import { ArtistInfo3 } from './ArtistInfo'
 
 function App() {
+/*
+  const Songs = [
+    { title: 'Te Perdio', id: 1, imageUrl: 'https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/ab/c2/1e/abc21e0c-3a9b-dc84-f69b-e28b441c788f/5054197891748.jpg/1000x1000bb.png'},
+    {}
+    
+  ]
+
+  let listSongs = Songs.map(song =>
+    <section key={song.id} id="artistList">
+      <div>
+        <img
+        className="cover"
+        src={song.imageUrl}
+        alt={song.title + " cover photo"}
+      />
+      <h2>{song.title}</h2>
+      <p>{song.date}</p>
+      </div>
+
+    </section>
+  );
+*/
   const[inputText, setInputText] = useState('')
   const[submitValue, setSubmitValue] = useState('')
 
@@ -17,33 +42,9 @@ function App() {
   return (
     <>
     <h1>Check out some of my favourites songs and artists!</h1>
-    <section 
-      id="body">
-    <section 
-      id="Artist">
-      <div
-        class="artist"> 
-        <a 
-          href="https://open.spotify.com/intl-pt/artist/14zUHaJZo1mnYtn6IBRaRP" >
-          <img src="https://l-hit.com/img_id/110913.jpg" />
-        </a>
-      </div>
-      <div class="artistText">  
-        <h2>Justin Quiles'</h2>
-        <h2>Genre: Reggaeton</h2>
-        <p>
-        Justin Rafael Quiles Rivera (born March 29, 1990), known professionally 
-        as Justin Quiles or J Quiles, is an American singer. 
-        Quiles has been nominated for Premios Juventud.
-        Besides writing his own songs, Quiles has written for his colleagues,
-        most notably "Pierde Los Modales", by J Balvin feat. Daddy Yankee, 
-        "Take It Off", by Lil Jon feat. Yandel & Becky G, "Recuérdame", by Maluma,
-        "Loba" and "Mi Nena", by Yandel and "Bichota", by Karol G where he also did backing vocals
-        Since August 2013, Quiles has been signed to record label Rich Music. 
-        In February 2017, Rich Music and Quiles signed a distribution deal with Warner Music's Latin division.
-        </p>
-      </div> 
-      </section>  
+    <ArtistInfo1/>
+    
+   
       <h2>Songs</h2>
       <section id="artistList">
         <div>
@@ -78,27 +79,7 @@ function App() {
         </div>
       </section>
       <hr></hr>
-    <section id="Artist">
-      <div class="artist"> 
-        <a href="https://open.spotify.com/intl-pt/artist/47MpMsUfWtgyIIBEFOr4FE" >
-          <img src="https://i.scdn.co/image/ab6761610000e5eb11ed192ea9e419dfaf801e9b" />
-        </a>
-      </div>
-      <div class="artistText">  
-        <h2>Lunay, also know as 'El Niño'</h2>
-        <h2>Genre: Reggaeton</h2>
-        <p>
-          Lunay is a Puerto Rican reggaeton artist whose charting collaborations with
-           artists such as Daddy Yankee, Anuel AA, Bad Bunny, and Zion & Lennox resulted 
-           in him scoring his own recording contract. Born in Puerto Rico in 2000, Lunay 
-           was far more interested in soccer than music as a young teen, but he caught the 
-           bug at 15. A year later, he was creating freestyle raps and uploading them to 
-           his Facebook and SoundCloud pages. Several of his audio clips went viral and 
-           caught the attention of producers Chris Jedi and Gaby Music, who signed him to 
-           a production deal on their Record Label - La Familia.
-        </p>
-      </div> 
-      </section>  
+      <ArtistInfo2/> 
       <h2>Songs</h2>
       <section id="artistList">
         <div>
@@ -131,24 +112,7 @@ function App() {
         </div>
       </section>
       <hr></hr>
-    <section id="Artist">
-      <div class="artist"> 
-        <a href="https://open.spotify.com/intl-pt/artist/47MpMsUfWtgyIIBEFOr4FE" >
-          <img src="https://i1.sndcdn.com/artworks-9P2e4uRYTO8QKbWV-oZDgFg-t500x500.jpg" />
-        </a>
-      </div>
-      <div class="artistText">  
-        <h2>Feid, also known as Ferxxo'</h2>
-        <h2>Genre: Reggaeton</h2>
-        <p>
-          Feid (Ferrxo) whose full real name (Birth Name) is Salomón Villada Hoyos 
-          is a very talented, popular, and versatile Colombian singer and songwriter.
-          Some of his lovely Songs are Yandel 150, Feliz Cumpleaños Ferxxo, LA INOCENTE,
-          Classy 101, Ferxxo 100, ULTRA SOLO, Normal, XXXX, FRIKI, TENGO FE, Castigo, 
-          BORRAXXA, Salir Con Vida, Como Ñengo & More.
-        </p>
-      </div> 
-      </section>  
+      <ArtistInfo3/> 
       <h2>Songs</h2>
       <section id="artistList">
         <div>
@@ -198,7 +162,7 @@ function App() {
       <div class="reviews">
 
       </div>
-      </section>
+      
     </>
   )
 }
---
+++ src/ArtistInfo.jsx
@@ -0,0 +1,113 @@
+export const ArtistInfo1 = () => {
+
+    return (
+        <>
+            <section 
+      id="Artist">
+      <div
+        class="artist"> 
+        <a 
+          href="https://open.spotify.com/intl-pt/artist/14zUHaJZo1mnYtn6IBRaRP" >
+          <img src="https://l-hit.com/img_id/110913.jpg" />
+        </a>
+      </div>
+      <div class="artistText">  
+        <h2>Justin Quiles'</h2>
+        <h2>Genre: Reggaeton</h2>
+        <p>
+        Justin Rafael Quiles Rivera (born March 29, 1990), known professionally 
+        as Justin Quiles or J Quiles, is an American singer. 
+        Quiles has been nominated for Premios Juventud.
+        Besides writing his own songs, Quiles has written for his colleagues,
+        most notably "Pierde Los Modales", by J Balvin feat. Daddy Yankee, 
+        "Take It Off", by Lil Jon feat. Yandel & Becky G, "Recuérdame", by Maluma,
+        "Loba" and "Mi Nena", by Yandel and "Bichota", by Karol G where he also did backing vocals
+        Since August 2013, Quiles has been signed to record label Rich Music. 
+        In February 2017, Rich Music and Quiles signed a distribution deal with Warner Music's Latin division.
+        </p>
+      </div> 
+      </section>
+        </>
+    )
+}
+
+export const ArtistInfo2 = () => {
+
+    return (
+        <>
+
+        <section id="Artist">
+        <div class="artist"> 
+          <a href="https://open.spotify.com/intl-pt/artist/47MpMsUfWtgyIIBEFOr4FE" >
+            <img src="https://i.scdn.co/image/ab6761610000e5eb11ed192ea9e419dfaf801e9b" />
+          </a>
+        </div>
+        <div class="artistText">  
+          <h2>Lunay, also know as 'El Niño'</h2>
+          <h2>Genre: Reggaeton</h2>
+          <p>
+            Lunay is a Puerto Rican reggaeton artist whose charting collaborations with
+             artists such as Daddy Yankee, Anuel AA, Bad Bunny, and Zion & Lennox resulted 
+             in him scoring his own recording contract. Born in Puerto Rico in 2000, Lunay 
+             was far more interested in soccer than music as a young teen, but he caught the 
+             bug at 15. A year later, he was creating freestyle raps and uploading them to 
+             his Facebook and SoundCloud pages. Several of his audio clips went viral and 
+             caught the attention of producers Chris Jedi and Gaby Music, who signed him to 
+             a production deal on their Record Label - La Familia.
+          </p>
+        </div> 
+        </section> 
+        </>
+    )
+}
+
+
+export const ArtistInfo3 = () => {
+
+    return (
+        <>
+
+<section id="Artist">
+      <div class="artist"> 
+        <a href="https://open.spotify.com/intl-pt/artist/47MpMsUfWtgyIIBEFOr4FE" >
+          <img src="https://i1.sndcdn.com/artworks-9P2e4uRYTO8QKbWV-oZDgFg-t500x500.jpg" />
+        </a>
+      </div>
+      <div class="artistText">  
+        <h2>Feid, also known as Ferxxo'</h2>
+        <h2>Genre: Reggaeton</h2>
+        <p>
+          Feid (Ferrxo) whose full real name (Birth Name) is Salomón Villada Hoyos 
+          is a very talented, popular, and versatile Colombian singer and songwriter.
+          Some of his lovely Songs are Yandel 150, Feliz Cumpleaños Ferxxo, LA INOCENTE,
+          Classy 101, Ferxxo 100, ULTRA SOLO, Normal, XXXX, FRIKI, TENGO FE, Castigo, 
+          BORRAXXA, Salir Con Vida, Como Ñengo & More.
+        </p>
+      </div> 
+      </section> 
+        </>)}
+/*
+<section 
+      id="Artist">
+      <div
+        class="artist"> 
+        <a 
+          href="https://open.spotify.com/intl-pt/artist/14zUHaJZo1mnYtn6IBRaRP" >
+          <img src="https://l-hit.com/img_id/110913.jpg" />
+        </a>
+      </div>
+      <div class="artistText">  
+        <h2>Justin Quiles'</h2>
+        <h2>Genre: Reggaeton</h2>
+        <p>
+        Justin Rafael Quiles Rivera (born March 29, 1990), known professionally 
+        as Justin Quiles or J Quiles, is an American singer. 
+        Quiles has been nominated for Premios Juventud.
+        Besides writing his own songs, Quiles has written for his colleagues,
+        most notably "Pierde Los Modales", by J Balvin feat. Daddy Yankee, 
+        "Take It Off", by Lil Jon feat. Yandel & Becky G, "Recuérdame", by Maluma,
+        "Loba" and "Mi Nena", by Yandel and "Bichota", by Karol G where he also did backing vocals
+        Since August 2013, Quiles has been signed to record label Rich Music. 
+        In February 2017, Rich Music and Quiles signed a distribution deal with Warner Music's Latin division.
+        </p>
+      </div> */
